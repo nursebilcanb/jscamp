f3aad1b6252c0521c740ae1546dffd31317f613b
nursebil canbolat <nursebiltw@gmail.com>
2021-10-19 00:02:27 +0300

    First Commit

---
+++ src/app.js
@@ -0,0 +1,16 @@
+console.log("Merhaba Kodlama.io")
+
+let dolarDun = 9.20
+let dolarBugun = 9.30
+dolarDun = "Ankara"
+
+{
+    let dolarDun = 9.10
+}
+
+console.log(dolarDun)
+
+const euroDun = 11.2
+
+
+console.log(euroDun)
