82c0aeb805564bb6743581e6f22f5faf21c8c853
nursebil canbolat <nursebiltw@gmail.com>
2021-11-23 17:29:39 +0300

    22 Kasım ders sonu

---
+++ src/data/users.js
@@ -0,0 +1,56 @@
+export const users = [
+    {
+        id: 1,
+        firstName: "Nursebil",
+        lastName: "Canbolat",
+        age: 20,
+        city:"Ankara",
+        creditCardNumber: "123456",
+        type: "customer"
+    },
+    {
+        id: 2,
+        firstName: "Bilal",
+        lastName: "Canbolat",
+        age:8,
+        city: "Ankara",
+        creditCardNumber: "654321",
+        type: "customer"
+    },
+    {
+        id: 3,
+        firstName: "Nisa",
+        lastName: "Canbolat",
+        city:"Ankara",
+        age:15,
+        salary: 8000,
+        type: "employee"
+    },
+    {
+        id: 4,
+        firstName: "Telli",
+        lastName: "Canbolat",
+        city: "Ankara",
+        age: 39,
+        salary: 8500,
+        type: "employee"
+    },
+    {
+        id: 4,
+        firstName: "Hasan",
+        lastName: "Canbolat",
+        city: "Ankara",
+        age: 44,
+        salary: 10000,
+        type: "employee"
+    },
+    {
+        id: 5,
+        firstName: "Engin",
+        lastName: "Demiroğ",
+        city: "Ankara",
+        age: 36,
+        salary: 10000,
+        type: "employee"
+    }
+]
